8e6cf2f549dbbc1be1225788b6166267f8a4102b
Michelle Parlevliet <113198354+michparlevliet@users.noreply.github.com>
2022-11-06 17:58:02 -0500

    Add files via upload

---
+++ Lab-6/lab6-2.js
@@ -0,0 +1,35 @@
+﻿//#### LAB 6 - DOM MANIPULATION ####
+//PART 2: MYSTERY BOX
+// alert("test");//COMMENT OUT AS SOON AS YOU KNOW YOU ARE CONNECTED!!!!
+
+//LISTEN FOR load EVENT
+window.onload = pageReady;
+function pageReady() {
+//'WRAPPER' FUNCTION FOR DOM LOGIC
+
+	//GET DOM ELEMENTS WE'LL NEED
+		var mysteryBox = document.getElementById("mysteryBox");
+		var buttonBox = document.getElementById("buttonBox");
+
+	//====CREATE THE FUNCTIONS WE'LL NEED====
+	//FUNCTION TO ASK USER
+	function askUser() {
+		var userResponse = confirm("Do you want to see something?");
+		if (userResponse === true) {
+			mysteryBox.style.display = "none";
+			buttonBox.style.display = "block";
+		}
+	}	
+
+	//FUNCTION TO CHANGE buttonBox
+	function changeButton() {
+		buttonBox.style.width = "600px";
+		buttonBox.innerHTML = "<h2>SURPRISE!!</h2>";
+		buttonBox.style.backgroundColor = "orange";
+	}
+
+	//SETUP LISTENERS
+	mysteryBox.onmouseover = askUser;
+	buttonBox.onclick = changeButton;
+
+}//END onload FUNCTION
